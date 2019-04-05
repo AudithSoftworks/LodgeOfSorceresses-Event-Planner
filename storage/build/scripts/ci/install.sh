fc19690e7bbfe5fd0541c8816ceb5bb6e38f8e47
Shahriyar Imanov <shehi@imanov.me>
2019-04-05 18:15:54 +0200

    Added opcache-reset script.

--- storage/build/scripts/ci/install.sh
+++ storage/build/scripts/ci/install.sh
@@ -10,5 +10,8 @@ composer install --prefer-source --no-interaction --no-dev;
 ./artisan route:cache;
 ./artisan storage:link
 
+touch ./public/$1.php;
+echo "<?php opcache_reset(); unlink(__FILE__);" | tee ./public/$1.php;
 rm /opt/lodgeofsorceresses/subdomains/events/current;
 ln -s /opt/lodgeofsorceresses/subdomains/events/$1 /opt/lodgeofsorceresses/subdomains/events/current;
+wget -q http://events.lodgeofsorceresses.com/$1.php
