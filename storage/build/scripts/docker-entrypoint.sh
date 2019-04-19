48cea4b179b4761196edf07162b0a2e09aa218cc
Shahriyar Imanov <shehi@imanov.me>
2019-04-19 11:52:43 +0200

    Attempt to fix Travis.

--- storage/build/scripts/docker-entrypoint.sh
+++ storage/build/scripts/docker-entrypoint.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-# this script must be called with root permissions
-if [[ $(id -g audith) != $MYGID || $(id -u audith) != $MYUID ]]; then
-    sudo groupmod -g $MYGID audith
-    sudo usermod -u $MYUID -g $MYGID audith
-fi;
-
-sudo mkdir -p /home/audith
-sudo chown -R audith:audith /home/audith
+sudo chmod +x ./$(dirname "$0")/fix-container-uid-gid.sh
+sudo ./$(dirname "$0")/fix-container-uid-gid.sh $MYUID $MYGID $@
 
 docker-php-entrypoint "$@"
-
