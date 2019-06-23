63a2d06240967b73bf69754a0ee0347f35496e88
Shahriyar Imanov <shehi@imanov.me>
2019-06-23 14:52:48 +0200

    Removed obsolete code from HomeController.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,6 @@
 <?php namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Services\IpsApi;
 
 class HomeController extends Controller
 {
@@ -12,19 +11,6 @@ class HomeController extends Controller
     public function index()
     {
         $this->authorize('user', User::class);
-
-        /** @var \App\Models\User $me */
-        $me = app('auth.driver')->user();
-        $me->load('linkedAccounts');
-        $oauthAccount = $me->linkedAccounts()->where('remote_provider', 'ips')->first();
-        if ($oauthAccount) {
-            $oauthAccount->remote_secondary_groups = explode(',', $oauthAccount->remote_secondary_groups);
-            if ($oauthAccount && $oauthAccount->remote_primary_group === IpsApi::MEMBER_GROUPS_SOULSHRIVEN) {
-                return redirect('https://lodgeofsorceresses.com');
-            }
-        } else {
-            return redirect('https://lodgeofsorceresses.com');
-        }
 
         return view('index');
     }
