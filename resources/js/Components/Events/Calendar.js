ded3e988318d8dc8cb7c3ba89c8f49c5f0206683
Shahriyar Imanov <shehi@imanov.me>
2019-08-25 07:40:09 +0200

    Added keys to children of Calendar component.

--- resources/js/Components/Events/Calendar.js
+++ resources/js/Components/Events/Calendar.js
@@ -2,9 +2,9 @@ import(
     /* webpackPrefetch: true */
     /* webpackChunkName: "calendar-scss" */
     '../../../sass/_calendar.scss'
-    );
+);
 
-import moment from "moment";
+import moment from 'moment';
 import React, { PureComponent } from 'react';
 
 export class Month extends PureComponent {
@@ -12,9 +12,7 @@ export class Month extends PureComponent {
         const headings = ['', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
         const headingsRendered = [];
         for (let i = 0; i < headings.length; i++) {
-            headingsRendered.push(
-                <th>{headings[i]}</th>
-            );
+            headingsRendered.push(<th key={'heading-' + i}>{headings[i]}</th>);
         }
 
         return headingsRendered;
@@ -29,44 +27,42 @@ export class Month extends PureComponent {
         const aMonthRendered = [];
         let startingNumberOfGaps = date.isoWeekday() - 1;
         if (startingNumberOfGaps) {
-            aWeekRendered.push(
-                <td colSpan={startingNumberOfGaps} />
-            );
+            aWeekRendered.push(<td key="starting-gap" colSpan={startingNumberOfGaps} />);
         }
         for (let i = 1; i <= daysInMonth; i++) {
             if (date.isoWeekday() === 1 && aWeekRendered.length) {
                 aMonthRendered.push(
-                    <tr>
-                        <td className='week-number'>W{date.isoWeek() - 1}</td>
+                    <tr key={'week-row-' + (date.isoWeek() - 1)}>
+                        <td key={'week-label-' + (date.isoWeek() - 1)} className="week-number">
+                            W{date.isoWeek() - 1}
+                        </td>
                         {aWeekRendered}
                     </tr>
                 );
                 aWeekRendered = [];
             }
-            aWeekRendered.push(
-                <td className='days' data-date={date.format('DD')}/>
-            );
+            aWeekRendered.push(<td key={'date-' + date.format('DD')} className="days" data-date={date.format('DD')} />);
             if (date.date() !== daysInMonth) {
                 date.add(1, 'days');
             }
         }
         let endingNumberOfGaps = 7 - date.isoWeekday();
         if (endingNumberOfGaps) {
-            aWeekRendered.push(
-                <td colSpan={endingNumberOfGaps} />
-            );
+            aWeekRendered.push(<td key="ending-gap" colSpan={endingNumberOfGaps} />);
         }
         if (aWeekRendered.length) {
             aMonthRendered.push(
-                <tr>
-                    <td className='week-number'>W{date.isoWeek()}</td>
+                <tr key={'week-row-' + date.isoWeek()}>
+                    <td key={'week-label-' + date.isoWeek()} className="week-number">
+                        W{date.isoWeek()}
+                    </td>
                     {aWeekRendered}
                 </tr>
             );
         }
 
         return (
-            <table className='calendar month-view col-lg-24'>
+            <table className="calendar month-view col-lg-24">
                 <thead>
                     <tr>{this.renderHeadings()}</tr>
                 </thead>
