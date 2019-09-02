383335a938aedd0aa4b3e87dd6f6f612c6337e52
Shahriyar Imanov <shehi@imanov.me>
2019-09-02 16:56:01 +0200

    Added Discord account linking status check to Dashboard.

--- resources/js/Components/Users/Name.js
+++ resources/js/Components/Users/Name.js
@@ -13,17 +13,15 @@ class Name extends PureComponent {
         return putUserAction(data);
     };
 
-    renderUpdateNameForm = () => {
-        return (
-            <form className="jumbotron danger ml-2 mr-2" onSubmit={this.handleSubmit} key="update-name-form">
-                <input type="hidden" name="_token" value={document.querySelector('meta[name="csrf-token"]').getAttribute('content')} />
-                <h3>Your ESO ID:</h3>
-                <input type='text' name='name' required placeholder='e.g. Glevissig (Gelmir)' />
-                <input type='submit' value='Save' className='btn btn-info' />
-                <small>Enter your ESO ID exactly as it is (omit @ sign). Feel free to add an easy-to-pronounce nickname for yourself (to be called with) in parentheses.</small>
-            </form>
-        );
-    };
+    renderUpdateNameForm = () => (
+        <form className="jumbotron danger ml-2 mr-2" onSubmit={this.handleSubmit} key="update-name-form">
+            <input type="hidden" name="_token" value={document.querySelector('meta[name="csrf-token"]').getAttribute('content')} />
+            <h3>Your ESO ID:</h3>
+            <input type='text' name='name' required placeholder='e.g. Glevissig (Gelmir)' />
+            <input type='submit' value='Save' className='btn btn-info' />
+            <small>Enter your ESO ID exactly as it is (omit @ sign). Feel free to add an easy-to-pronounce nickname for yourself (to be called with) in parentheses.</small>
+        </form>
+    );
 
     renderName = user => (
         <article className='jumbotron success ml-2 mr-2'>
--- resources/js/Controllers/Home.js
+++ resources/js/Controllers/Home.js
@@ -8,11 +8,11 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faDiscord } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
-import React, { Fragment, PureComponent } from 'react';
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import { errorsAction } from '../actions/notifications';
 import Notification from '../Components/Notification';
+import DiscordOauthAccount from "../Components/Users/DiscordOauthAccount";
 import Name from "../Components/Users/Name";
 import { user } from '../vendor/data';
 
@@ -23,71 +23,17 @@ class Home extends PureComponent {
         this.props.axiosCancelTokenSource && this.props.axiosCancelTokenSource.cancel('Unmount');
     };
 
-    renderActionList = associatedDiscordAccount => {
-        let discordLink = (
-            <a href="/oauth/to/discord" className="ne-corner danger" title="Discord not linked!">
-                <FontAwesomeIcon icon={['fab', 'discord']} />
-            </a>
-        );
-        if (associatedDiscordAccount) {
-            discordLink = (
-                <a className="ne-corner success" title="Discord linked!">
-                    <FontAwesomeIcon icon={['fab', 'discord']} />
-                </a>
-            );
-        }
-        const actionList = {
-            create: discordLink,
-        };
-        let actionListRendered = [];
-        for (const [actionType, link] of Object.entries(actionList)) {
-            if (link) {
-                actionListRendered.push(<li key={actionType}>{link}</li>);
-            }
-        }
-        return actionListRendered;
-    };
-
-    renderDiscordNotLinkedNotification = () => {
-        const { me, notifications } = this.props;
-        if (me && !me.linkedAccountsParsed.discord && notifications.find(n => n.key === 'no-discord-account-linked') === undefined) {
-            const message = [
-                <Fragment key="f-1">
-                    <b>Discord not linked!</b> You won't be able to use Planner unless this issue is addressed. Click
-                </Fragment>,
-                <FontAwesomeIcon icon={['fab', 'discord']} key="icon" />,
-                <Fragment key="f-2">icon to the top right, to fix this.</Fragment>,
-            ].reduce((prev, curr) => [prev, ' ', curr]);
-            this.props.dispatch(
-                errorsAction(
-                    message,
-                    {
-                        container: 'bottom-center',
-                        animationIn: ['animated', 'bounceInDown'],
-                        animationOut: ['animated', 'bounceOutDown'],
-                        dismiss: { duration: 30000 },
-                        width: 350,
-                    },
-                    'no-discord-account-linked'
-                )
-            );
-        }
-    };
-
     render = () => {
         const { me, location } = this.props;
         if (!me) {
             return <Redirect to={{ pathname: '/', state: { prevPath: location.pathname } }} />;
         }
-        this.renderDiscordNotLinkedNotification();
-        const actionListRendered = this.renderActionList(me.linkedAccountsParsed.discord);
-
         return [
             <section className="col-md-24 p-0 mb-4" key="dashboard">
                 <h2 className="form-title col-md-24" title="Account Status">
                     Account Status
                 </h2>
-                <ul className="ne-corner">{actionListRendered}</ul>
+                <DiscordOauthAccount />
                 <Name />
             </section>,
             <Notification key="notifications" />,
