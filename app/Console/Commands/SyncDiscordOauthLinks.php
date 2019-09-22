2d15ede1b8e268ef9779c88f2ef0d41a1dd1e29c
Şəhriyar İmanov <shehi@imanov.me>
2019-09-23 01:08:51 +0200

    Revert

--- app/Console/Commands/SyncDiscordOauthLinks.php
+++ app/Console/Commands/SyncDiscordOauthLinks.php
@@ -46,7 +46,7 @@ class SyncDiscordOauthLinks extends Command
      * @return bool
      * @throws \Exception
      */
-    public function handle(): void
+    public function handle(): bool
     {
         /** @var UserOAuth|\Illuminate\Database\Eloquent\Collection $oauthAccount */
         $oauthAccounts = UserOAuth::whereRemoteProvider('discord')->get();
@@ -91,5 +91,7 @@ class SyncDiscordOauthLinks extends Command
             $oauthAccount = $oauthAccounts->shift();
         }
         $this->info('Discord OAuth accounts successfully synced!');
+        
+        return true;
     }
 }
