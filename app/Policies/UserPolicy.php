00c438544554537349c0a916d1a9e53777a1c952
Shahriyar Imanov <shehi@imanov.me>
2019-11-01 20:10:24 +0100

    Admin authorization is Discord based now, instead of IPS.

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -3,7 +3,6 @@
 namespace App\Policies;
 
 use App\Services\DiscordApi;
-use App\Services\IpsApi;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class UserPolicy
@@ -19,37 +18,30 @@ class UserPolicy
     {
         /** @var \App\Models\User $me */
         $me = app('auth.driver')->user();
-        $me->loadMissing('linkedAccounts');
-        $oauthAccount = $me->linkedAccounts()->where('remote_provider', 'ips')->first();
-        if ($oauthAccount && !empty($oauthAccount->remote_secondary_groups)) {
-            $oauthAccount->remote_secondary_groups = explode(',', $oauthAccount->remote_secondary_groups);
+        $oauthAccount = $me->linkedAccounts()->where('remote_provider', 'discord')->first();
+        if ($oauthAccount) {
+            $oauthAccount->remote_secondary_groups = !empty($oauthAccount->remote_secondary_groups)
+                ? explode(',', $oauthAccount->remote_secondary_groups)
+                : [];
+
+            return in_array(DiscordApi::ROLE_MAGISTER_TEMPLI, $oauthAccount->remote_secondary_groups, true);
         }
 
-        return $oauthAccount
-            && $oauthAccount->remote_primary_group !== IpsApi::MEMBER_GROUPS_SOULSHRIVEN
-            && (
-                $oauthAccount->remote_primary_group === IpsApi::MEMBER_GROUPS_IPSISSIMUS
-                || $oauthAccount->remote_primary_group === IpsApi::MEMBER_GROUPS_MAGISTER_TEMPLI
-                || in_array(IpsApi::MEMBER_GROUPS_MAGISTER_TEMPLI, $oauthAccount->remote_secondary_groups, false)
-            );
+        return false;
     }
 
     public function user(): bool
     {
         /** @var \App\Models\User $me */
         $me = app('auth.driver')->user();
-        $me->loadMissing('linkedAccounts');
         $oauthAccount = $me->linkedAccounts()->where('remote_provider', 'discord')->first();
         if ($oauthAccount) {
-            if (!empty($oauthAccount->remote_secondary_groups)) {
-                $oauthAccount->remote_secondary_groups = explode(',', $oauthAccount->remote_secondary_groups);
-            }
+            $oauthAccount->remote_secondary_groups = !empty($oauthAccount->remote_secondary_groups)
+                ? explode(',', $oauthAccount->remote_secondary_groups)
+                : [];
 
-            return !empty($oauthAccount->remote_secondary_groups)
-                && (
-                    in_array(DiscordApi::ROLE_SOULSHRIVEN, $oauthAccount->remote_secondary_groups, true)
-                    || in_array(DiscordApi::ROLE_MEMBERS, $oauthAccount->remote_secondary_groups, true)
-                );
+            return in_array(DiscordApi::ROLE_SOULSHRIVEN, $oauthAccount->remote_secondary_groups, true)
+                || in_array(DiscordApi::ROLE_MEMBERS, $oauthAccount->remote_secondary_groups, true);
         }
 
         return false;
