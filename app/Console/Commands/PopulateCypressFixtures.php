f73c5c049035b43f734219da364ab17776c962dc
Shahriyar Imanov <shehi@imanov.me>
2020-01-16 11:31:24 +0100

    Small CS cleanup.

--- app/Console/Commands/PopulateCypressFixtures.php
+++ app/Console/Commands/PopulateCypressFixtures.php
@@ -2,10 +2,8 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Character;
-use Illuminate\Cache\Repository;
 use Illuminate\Console\Command;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Cache;
 
 class PopulateCypressFixtures extends Command
 {
@@ -21,18 +19,18 @@ class PopulateCypressFixtures extends Command
      *
      * @var string
      */
-    protected $description = 'Warms-up application cache.';
+    protected $description = 'Populates JSON fixtures for Cypress E2E testing.';
 
     public function handle(): void
     {
         $this->info('Starting populating Cypress fixtures...');
-        $cacheService = app('cache.store');
-        $cacheService->has('sets'); // has() triggers CacheMissed event, which in return triggers Recache mechanism.
-        $setsCacheAsJson = json_encode($cacheService->get('sets'), JSON_OBJECT_AS_ARRAY);
+
+        Cache::has('sets'); // has() triggers CacheMissed event, which in return triggers Recache mechanism.
+        $setsCacheAsJson = json_encode(Cache::get('sets'), JSON_OBJECT_AS_ARRAY);
         file_put_contents(base_path('./cypress/fixtures/.sets.json'), $setsCacheAsJson);
 
-        $cacheService->has('skills');
-        $skillsCacheAsJson = json_encode($cacheService->get('skills'), JSON_OBJECT_AS_ARRAY);
+        Cache::has('skills');
+        $skillsCacheAsJson = json_encode(Cache::get('skills'), JSON_OBJECT_AS_ARRAY);
         file_put_contents(base_path('./cypress/fixtures/.skills.json'), $skillsCacheAsJson);
 
         $this->info('Cypress fixture population complete.');
