7f8efa5952ec58feaebbe0233f940011c99f88f4
Shahriyar Imanov <shehi@imanov.me>
2020-06-13 01:53:47 +0200

    CS fix + Fixed AttendanceFactory.

--- database/factories/AttendanceFactory.php
+++ database/factories/AttendanceFactory.php
@@ -6,6 +6,8 @@ use App\Models\Attendance;
 $factory->define(Attendance::class, static function (\Faker\Generator $faker) {
     return [
         'text' => $faker->text,
+        'text_for_forums' => $faker->text,
+        'text_for_planner' => $faker->text,
         'discord_message_id' => $faker->numberBetween(),
         'gallery_image_ids' => null,
     ];
