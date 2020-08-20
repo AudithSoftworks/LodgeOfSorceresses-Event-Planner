592402d70efcca8b6f46a6078ef94e9fc78131b8
Shahriyar Imanov <shehi@imanov.me>
2020-08-20 15:29:38 +0200

    Enhanced AttendanceControllerTest.

--- tests/Integration/JsonApi/AttendanceControllerTest.php
+++ tests/Integration/JsonApi/AttendanceControllerTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\Integration\JsonApi;
 use App\Models\Attendance;
 use App\Tests\IlluminateTestCase;
 use App\Tests\Integration\JsonApi\Traits\NeedsUserStubs;
+use Carbon\CarbonImmutable;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Artisan;
 
@@ -34,12 +35,33 @@ class AttendanceControllerTest extends IlluminateTestCase
         $response->assertJsonPath('message', 'This action is unauthorized.');
     }
 
+    /**
+     * @throws \Exception
+     */
     public function testIndexForSuccess(): void
     {
         $this->stubSoulshrivenUser();
 
         /** @var Attendance $attendance */
-        $attendance = factory(Attendance::class)->create();
+        $attendance = factory(Attendance::class)->create([
+            'text' => '2 weeks ago',
+            'created_at' => (new CarbonImmutable())->subWeeks(2),
+        ]);
+        $attendance->attendees()->sync([static::$soulshriven->id]);
+        $attendance = factory(Attendance::class)->create([
+            'text' => '4 weeks ago',
+            'created_at' => (new CarbonImmutable())->subWeeks(4),
+        ]);
+        $attendance->attendees()->sync([static::$soulshriven->id]);
+        $attendance = factory(Attendance::class)->create([
+            'text' => '4 weeks 1 day ago',
+            'created_at' => (new CarbonImmutable())->subWeeks(4)->subDay(),
+        ]);
+        $attendance->attendees()->sync([static::$soulshriven->id]);
+        $attendance = factory(Attendance::class)->create([
+            'text' => '6 weeks ago',
+            'created_at' => (new CarbonImmutable())->subWeeks(5),
+        ]);
         $attendance->attendees()->sync([static::$soulshriven->id]);
 
         $response = $this
@@ -52,10 +74,45 @@ class AttendanceControllerTest extends IlluminateTestCase
         static::assertNotNull($responseOriginalContent);
         static::assertIsIterable($responseOriginalContent);
         static::assertCount(1, $responseOriginalContent);
-        $responseAsArray = $responseOriginalContent->toArray();
-        $firstEntry = $responseAsArray[0];
-        static::assertNotEmpty($firstEntry['attendees']);
-        static::assertIsIterable($firstEntry['attendees']);
-        static::assertCount(1, $firstEntry['attendees']);
+        $entries = $responseOriginalContent->toArray();
+        static::assertEquals('2 weeks ago', $entries[0]['text']);
+        static::assertNotEmpty($entries[0]['attendees']);
+        static::assertIsIterable($entries[0]['attendees']);
+        static::assertCount(1, $entries[0]['attendees']);
+
+        $createdAtInAtomFormat = (new CarbonImmutable($entries[0]['created_at']))->toAtomString();
+        $response = $this
+            ->actingAs(static::$soulshriven)
+            ->withoutMiddleware()
+            ->getJson('/api/attendances/' . static::$soulshriven->id . '?b=' . urlencode($createdAtInAtomFormat));
+        $response->assertStatus(JsonResponse::HTTP_OK);
+        /** @var \Illuminate\Support\Collection $responseOriginalContent */
+        $responseOriginalContent = $response->getOriginalContent();
+        static::assertNotNull($responseOriginalContent);
+        static::assertIsIterable($responseOriginalContent);
+        static::assertCount(2, $responseOriginalContent);
+        $entries = $responseOriginalContent->toArray();
+        static::assertEquals('4 weeks ago', $entries[0]['text']);
+        static::assertEquals('4 weeks 1 day ago', $entries[1]['text']);
+        static::assertNotEmpty($entries[0]['attendees']);
+        static::assertIsIterable($entries[0]['attendees']);
+        static::assertCount(1, $entries[0]['attendees']);
+
+        $createdAtInAtomFormat = (new CarbonImmutable($entries[1]['created_at']))->toAtomString();
+        $response = $this
+            ->actingAs(static::$soulshriven)
+            ->withoutMiddleware()
+            ->getJson('/api/attendances/' . static::$soulshriven->id . '?b=' . urlencode($createdAtInAtomFormat));
+        $response->assertStatus(JsonResponse::HTTP_OK);
+        /** @var \Illuminate\Support\Collection $responseOriginalContent */
+        $responseOriginalContent = $response->getOriginalContent();
+        static::assertNotNull($responseOriginalContent);
+        static::assertIsIterable($responseOriginalContent);
+        static::assertCount(1, $responseOriginalContent);
+        $entries = $responseOriginalContent->toArray();
+        static::assertEquals('6 weeks ago', $entries[0]['text']);
+        static::assertNotEmpty($entries[0]['attendees']);
+        static::assertIsIterable($entries[0]['attendees']);
+        static::assertCount(1, $entries[0]['attendees']);
     }
 }
